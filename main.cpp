b01882d319eff0e3b3a3ea31afdf0b7139d0d97c
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-04-01 14:50:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "Selectivity.hpp"
 #include "Recruitment.hpp"
 #include "Mortality.hpp"
+#include "Data.hpp"
 
 int main(int argc, const char * argv[]) {
     selectivity::logistic<double> myselectivity(5.0,0.7); //replaces T with double
@@ -21,6 +22,22 @@ int main(int argc, const char * argv[]) {
     std::cout<<myrecruitment2.evaluate(150,50)<<std::endl;
     
     mortality::test_mortality();
+    
+    data::agecomp_data<double> myagecomp(10,10);
+    for(int i = 0; i<myagecomp.imax_m; i++){
+        for(int j=0; j<myagecomp.jmax_m; j++){
+            myagecomp(i,j)=3.1459;
+            std::cout<<myagecomp(i,j)<<std::endl;
+        }
+    }
+
+    data::index_data<double> d(10);
+    std::cout<<d.imax_m<<"\n";
+    for(int i =0; i < 10; i++){
+        d(i) = 3.1459;
+        std::cout<<d(i)<<"\n";
+    }
+    
     
     return 0;
 }
