1d3ecc1cce48f469bb81be7b7ec4e8cdd1b3c90d
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-04-13 14:04:52 -0400

    Update Survey.hpp

--- Survey.hpp
+++ Survey.hpp
@@ -2,11 +2,41 @@
 //  Survey.hpp
 //  Catch_at_Age
 //
-//  Created by Haley  Oleynik  on 3/16/21.
 //
 
 #ifndef Survey_h
 #define Survey_h
 
+#include "Common.hpp"
+#include "Data.hpp"
+#include "Selectivity.hpp"
+
+namespace survey{
+
+template<class T>
+ class survey: public model_base<T>{
+  public:
+    population<T>* population;
+    index_data<T>* observed_index_data;
+    agecomp_data<T>* observed_agecomp_data;
+    
+    //derived quantities
+    std::vector<T> survey_at_age;
+    std::vector<T> derived_index_data;
+    std::vector<T> derived_agecomp_data;
+    
+    //operators
+    selectivity_base* selectivity_model;
+    T q
+     
+     void evaluate(){
+         
+     }
+    
+}
+
+}
+
 
 #endif /* Survey_h */
+
