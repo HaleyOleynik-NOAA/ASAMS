9bbea485d76b1ef58f620ee119454b40d6b00060
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-04-13 14:04:14 -0400

    Update Fleet.hpp

--- Fleet.hpp
+++ Fleet.hpp
@@ -2,11 +2,43 @@
 //  Fleet.hpp
 //  Catch_at_Age
 //
-//  Created by Haley  Oleynik  on 3/16/21.
 //
 
 #ifndef Fleet_h
 #define Fleet_h
 
+
+#include "Common.hpp"
+#include "Selectivity.hpp"
+#include "Data.hpp"
+#include "Mortality.hpp"
+
+namespace fleet{
+
+template<class T>
+ class fleet: public model_base<T>{
+  public:
+    population<T>* population;
+    index_data<T>* observed_index_data;
+    agecomp_data<T>* observed_agecomp_data;
+    
+    //derived quantities
+    std::vector<T> landings;
+    std::vector<T> catch_at_age;
+    std::vector<T> z_at_age;
+    std::vector<T> derived_index_data;
+    std::vector<T> derived_agecomp_data;
+    
+    //operators
+    selectivity_base* selectivity_model;
+    std::vector<T> fishing_mortality;
+     
+     void evaluate(){
+         
+     }
+    
+}
+
+}
 
 #endif /* Fleet_h */
