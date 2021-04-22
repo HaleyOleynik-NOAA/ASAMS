810303b8d889a658bd5dafb8763eadb6fed07443
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-04-22 14:33:41 -0400

    Update Population.hpp

--- Population.hpp
+++ Population.hpp
@@ -11,6 +11,9 @@
 #include "Growth.hpp"
 #include "Recruitment.hpp"
 #include "Mortality.hpp"
+#include "Survey.hpp"
+#include "Fleet.hpp"
+
 
 namespace population{
   
@@ -24,13 +27,13 @@ namespace population{
     std::vector<T> numbers_at_age;
     std::vector<T> weight_at_age;
     std::vector<T> ssb;
-    std::vectpr<T> abundance;
-    std::vectpr<T> biomass;
+    std::vector<T> abundance;
+    std::vector<T> biomass;
     
     //life history submodels
     int nyears;
     int nages;
-    std::vectpr<T> ages;
+    std::vector<T> ages;
     growth_base* growth_model;
     recruitment_base* recruitment_model;
     mortality_base* mortality_model;
@@ -39,7 +42,6 @@ namespace population{
       
     }
     
-    
     void initialize(int nyears, int nages){          // initialize function
       
     }
@@ -47,37 +49,35 @@ namespace population{
     void prepare(){                                 // reset all values in containers
     }
     
-    void calculate_mortality(){
+    void calculate_mortality(int year, std::vector<fleet*>& fleets){
     }
     
-    void calculate_maturity(){
+    void calculate_maturity(int year){
     }
     
     void calculate_initial_numbers_at_age(){
     }
     
-    void calculate_numbers_at_age(){
+    void calculate_numbers_at_age(int year){
     }
     
-    void calculate_growth(){
+    void calculate_growth(int year){
     }
     
-    void calculate_stock_spawning_biomass(){
+    void calculate_spawning_stock_biomass(int year){
     }
     
-    void calculate_recruitment(){
+    void calculate_recruitment(int year){
     }
     
-    void calculate_catch_at_age(){
+    void calculate_catch_at_age(int year, std::vector<fleet*>& fleets){
     }
     
-    void calculate_survey_numbers_at_age(){
+    void calculate_survey_numbers_at_age(int year, std::vector<survey*>& surveys){
     }
     
-    void evaluate(){
-      
-    }
   }
 }
 
 #endif /* Population_h */
+
