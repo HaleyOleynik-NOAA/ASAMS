6cb02e230439ef54c69f7ff52f451c13bca37e52
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-04-30 09:28:43 -0400

    Update Fleet.hpp

--- Fleet.hpp
+++ Fleet.hpp
@@ -1,8 +1,5 @@
-//
 //  Fleet.hpp
-//  Catch_at_Age
-//
-//
+//  Age Structured Assessment Modeling System (ASAMS)
 
 #ifndef Fleet_h
 #define Fleet_h
@@ -12,14 +9,12 @@
 #include "Selectivity.hpp"
 #include "Data.hpp"
 #include "Mortality.hpp"
-#include "Population.hpp"
 
 namespace asams{
 
 template<class T>
  class fleet: public model_base<T>{
   public:
-    population<T>* population;
     index_data<T>* observed_index_data;
     agecomp_data<T>* observed_agecomp_data;
     
@@ -29,10 +24,41 @@ template<class T>
     std::vector<T> z_at_age;
     std::vector<T> derived_index_data;
     std::vector<T> derived_agecomp_data;
+     int nyears;
+     int nages;
+     
     
     //operators
     selectivity_base<T>* selectivity_model;
     std::vector<T> fishing_mortality;
+     
+     fleet(int nyears, int nages ){  // constructor
+         this-> nyears = nyears;
+         this-> nages = nages;
+         
+         landings.resize(nyears);
+         catch_at_age.resize(nyears*nages);
+         z_at_age.resize(nyears*nages);
+         derived_index_data.resize(nyears);
+         derived_agecomp_data.resize(nyears*nages);
+         fishing_mortality.resize(nyears);
+     }
+     
+     void prepare(){                                 // reset all values in containers
+         for(int i = 0; i < nyears; i++){
+             landings[i]=0;
+             fishing_mortality[i]=0;
+             derived_index_data[i]=0;
+             
+             for(int j = 0; j < nages; j++){
+                 int index = i*nages+j;
+                 catch_at_age[index]=0;
+                 derived_agecomp_data[index]=0;
+                 z_at_age[index]=0;
+             }
+             
+         }
+     }
      
      T likelihood(){
          
