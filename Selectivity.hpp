78c9a22d21c97557e817f5ff87262ad830c2cb10
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-06-08 09:20:06 -0400

    Added doxygen comments

--- Selectivity.hpp
+++ Selectivity.hpp
@@ -8,6 +8,10 @@
 
 namespace asams{
 
+/**
+ *Selectivity base class. Inherits from modelbase.
+ */
+
 template <class T>
 class selectivity_base:public model_base<T>{
 protected:
@@ -16,25 +20,46 @@ public:
     virtual T evaluate(const T& age)=0;
 };
 
+/**
+ *Logistic selectivity implementation. Inherits from selectivity base.
+ */
+
 template <class T>
-class logistic:public selectivity_base<T>{
+class logistic_selectivity:public selectivity_base<T>{
     
 public:
-    T a50_m; // m means member of class
+    T a50_m; // m denotes member of class
     T slope_m;
     
-    logistic(){
-        
+    logistic_selectivity(){
+    
     }
     
-    logistic(T a50, T slope){  // constructor
+    /**
+     *Constructor that takes arguments
+     *@param a50 Age at which selectivity = 50%
+     *@param slope Slope
+     */
+    
+    logistic_selectivity(T a50, T slope){  // constructor
         this->a50_m=a50;
         this->slope_m=slope;
     }
     
-    ~ logistic(){    //destructor
+    /**
+     *Deconstructor
+     */
+    
+    ~ logistic_selectivity(){    //destructor
         std::cout <<"I just deleted"<<std::endl;
     }
+    
+    /**
+     *@brief Logistic selectivity implementation of the virtual evaluate function. Required to be overloaded.
+     *
+     *Logistic selectivity implementation \f$ \frac{1.0}{1.0+exp(-1.0*\frac{age-a50}{slope}}\f$
+     *@param age Age
+     */
     
     virtual T evaluate(const T& age){
         return (1.0) / (1.0 + asams::exp(-1.0 * (age - a50_m) / slope_m));
@@ -42,8 +67,12 @@ public:
     
 };
 
+/**
+ *Double logistic selectivity implementation. Inherits from selectivity base.
+ */
+
 template <class T>
-class doublelogistic:public selectivity_base<T>{
+class doublelogistic_selectivity:public selectivity_base<T>{
 
 public:
     T alpha_asc_m;
@@ -51,20 +80,40 @@ public:
     T alpha_desc_m;
     T beta_desc_m;
     
-    doublelogistic(){       // default constructor
+    doublelogistic_selectivity(){       // default constructor
         
     }
     
-        doublelogistic(T alpha_asc, T beta_asc,T alpha_desc,T beta_desc){
+    /**
+     *Constructor that takes arguments
+     *@param alpha_asc \f$ \alpha_1\f$ Inflection point of the first logistic curve
+     *@param beta_asc \f$ \beta_1\f$ Slope of the first logistic curve
+     *@param alpha_desc \f$ \alpha_2\f$ Inflection point of second logistic curve
+     *@param beta_desc \f$ \beta_2\f$ Slope of the second logistic curve
+     */
+    
+    doublelogistic_selectivity(T alpha_asc, T beta_asc,T alpha_desc,T beta_desc){
             this->alpha_asc_m=alpha_asc;
             this->beta_asc_m=beta_asc;
             this->alpha_desc_m=alpha_desc;
             this->beta_desc_m=beta_desc;
         }
     
-    ~doublelogistic(){
+    /**
+     *Deconstructor
+     */
+    
+    ~doublelogistic_selectivity(){
         std::cout <<"I just deleted"<< std::endl;
     }
+    
+    /**
+     *@brief Double logistic selectivity implementation of the virtual evaluate function. Required to be overloaded.
+     *
+     *Double logistic selectivity implementation\f$ \frac{1.0}{1.0+exp(-\beta_1*(age-\alpha_1)}*1-\frac{1}{1+exp(-\beta_2*(age-\alpha_2)}\f$
+     *@param age Age
+     */
+    
         virtual T evaluate(const T& age){
             return (1.0/ (1.0+asams::exp(-beta_asc_m * (age - alpha_asc_m)))) * (1.0- (1.0/ (1.0+asams::exp(-beta_desc_m * (age - alpha_desc_m)))));
         }
