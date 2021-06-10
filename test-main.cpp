1342f6839f45539da584551fd99837e7ab55e74f
ChristineStawitz-NOAA <christine.stawitz@noaa.gov>
2021-06-10 10:55:08 -0700

    add template def

--- test-main.cpp
+++ test-main.cpp
@@ -1,9 +1,9 @@
 #include "Common.hpp"
 #include "catch_amalgamated.hpp"
-using namespace asams;
 
-TEST_CASE( "Logistic of 100 is 1 (fail)", "[single-file]" ) {
-  logistic<T>* logisticCurve;
+template< typename T>
+TEMPLATE_TEST_CASE( "Logistic of 100 is 1 (fail)", "[single-file]" ) {
+  asams::logistic<T> logisticCurve(5,2);
   
-  REQUIRE( logisticCurve->evaluate(100) == 1 );
+  REQUIRE( logisticCurve.evaluate(100) == 1 );
 }
