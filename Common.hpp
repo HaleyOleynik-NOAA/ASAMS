6a4d4498fb60e94d541207413b0eab0e763fdbb5
Haley Oleynik <78555815+HaleyOleynik-NOAA@users.noreply.github.com>
2021-06-15 13:48:27 -0400

    Update Common.hpp

--- Common.hpp
+++ Common.hpp
@@ -27,39 +27,10 @@ template <class T>
 const T exp(const T& x){return std::exp(x);}
 
 template <class T>
-class logistic:public model_base<T>{
-    
-public:
-    T a50_m; // m means member of class
-    T slope_m;
-    
-    logistic(){
-        
-    }
-    
-    /// Sets the logistic parameters.
-    ///
-    /// @param[in] a50 The inflection age of the logistic curve.
-    /// @param[in] slope The logistic curve slope.
-    logistic(T a50, T slope){  // constructor
-        this->a50_m=a50;
-        this->slope_m=slope;
-    }
-    
-    ~ logistic(){    //destructor
-    }
-    
-    ///Creates an logistic curve with  \f$ Y = \frac{1.0}{1.0+\mathrm{exp}(-1(age-a_{50})/slope)}\f$
-    //
-    //@param[in] age the age at which to get the logistic curve output
-    //@return the value (between 0 and 1) of the curve at age \code{age}
-    virtual T evaluate(const T& age){
-        return (1.0) / (1.0 + asams::exp(-1.0 * (age - a50_m) / slope_m));
-    }
-    
-};
-
+inline const T logistic(const T& a50, const T& slope, const T& age){
+    return (1.0) / (1.0 + asams::exp(-1.0 * (age - a50) / slope));
 }
 
+}
 
 #endif /* Common_h */
